e09369161c5fd844b167950bce459c244d5d9ac2
jonathanhuang <jonathan95129@gmail.com>
2020-10-21 14:56:56 -0700

    intial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'; //different
+import logo from './logo.svg';
+import './App.css';
+import Board from './board'
+class App extends Component { //different
+  render() {
+    return (
+    <div className="App" id={'root'}>
+      <text>test</text>
+        <Board />
+    </div>
+    );
+  }
+};
+
+export default App;
---
+++ src/board.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import reactDOM from 'react-dom';
+class Board extends Component {
+
+    render() {
+        const row = 9;
+        const col = 9;
+
+        return (
+        <div>
+            <text>hi FINALLY</text>
+        </div>
+
+
+        );
+    }
+};
+export default Board;
